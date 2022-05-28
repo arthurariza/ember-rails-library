46bf35d38ebda96df267e7289e1b82effa65ad78
arthurariza <tucaariza@gmail.com>
2022-05-28 16:58:01 -0300

    feat: Author ember model

--- ui/app/routes/author.js
+++ ui/app/routes/author.js
@@ -1,7 +1,7 @@
 import Route from "@ember/routing/route";
 
 export default class Author extends Route {
-  model() {
-    return fetch("http://127.0.0.1:3000/authors").then((res) => res.json());
+  async model() {
+    return this.store.findAll("author");
   }
 }
