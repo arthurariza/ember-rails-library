63b7200f21abbd26b6e71ece24315dd48a9ac055
arthurariza <tucaariza@gmail.com>
2022-05-27 22:09:43 -0300

    feat: Fetch and Display Authors

--- ui/app/routes/author.js
+++ ui/app/routes/author.js
@@ -2,9 +2,6 @@ import Route from "@ember/routing/route";
 
 export default class Author extends Route {
   model() {
-    return [
-      { first: "J.K.", last: "Rowling" },
-      { first: "Tom", last: "Clancy" },
-    ];
+    return fetch("http://127.0.0.1:3000/authors").then((res) => res.json());
   }
 }
