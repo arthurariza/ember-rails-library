5a72c7fdb610d269f8fed5ee1a44fcc8e99ce794
arthurariza <tucaariza@gmail.com>
2022-05-28 17:05:47 -0300

    feat: Dynamic authors route

--- ui/app/router.js
+++ ui/app/router.js
@@ -1,11 +1,13 @@
-import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import EmberRouter from "@ember/routing/router";
+import config from "./config/environment";
 
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
-Router.map(function() {
-  this.route('author');
+Router.map(function () {
+  this.route("author", { path: "authors" }, function () {
+    this.route("details", { path: "/:id" });
+  });
 });
