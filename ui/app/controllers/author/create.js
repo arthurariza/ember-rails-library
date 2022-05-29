0b84b1b8194e7a61354ac396542110752075ab9f
arthurariza <tucaariza@gmail.com>
2022-05-29 19:27:22 -0300

    feat: Create Author form submit

--- ui/app/controllers/author/create.js
+++ ui/app/controllers/author/create.js
@@ -1,3 +1,15 @@
 import Controller from "@ember/controller";
+import { action } from "@ember/object";
 
-export default class AuthorCreateController extends Controller {}
+export default class AuthorCreateController extends Controller {
+  @action
+  async createAuthor(e) {
+    e.preventDefault();
+
+    const author = this.store.createRecord("author", this.model);
+
+    await author.save();
+
+    this.transitionToRoute("author");
+  }
+}
